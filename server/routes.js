416242e02f3b5bae495dbb706a557c094fe0cdaa
Luis Angel Santos <santosangel748@gmail.com>
2022-10-28 11:35:56 -0500

    API REST Get Mensajes

--- server/routes.js
+++ server/routes.js
@@ -2,17 +2,20 @@
 var express = require('express');
 var router = express.Router();
 
+const mensajes = [{
+
+  _id: 'XXX',
+  user: 'spiderman',
+  mensaje: 'Hola mundo'
 
 
-
-
-
+}, ];
 
 // Get mensajes
-router.get('/', function (req, res) {
-  res.json('Obteniendo mensajes');
+router.get('/', function(req, res) {
+  // res.json('Obteniendo mensajes');
+  res.json(mensajes);
 });
-
 
 
 
